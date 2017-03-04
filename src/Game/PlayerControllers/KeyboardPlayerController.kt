1966b626a0e29a01b98780e2a9ad23b019755fe7
woitee <woitee@gmail.com>
2017-03-04 18:59:04 +0100

    Added simple keyboard controls

---
+++ src/Game/PlayerControllers/KeyboardPlayerController.kt
@@ -0,0 +1,65 @@
+package Game.PlayerControllers
+
+/**
+ * Created by woitee on 04/03/2017.
+ */
+
+import Game.GameActions.IGameAction
+import Game.GameActions.JumpAction
+import Game.GameState
+import Game.Game
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
+import java.util.*
+
+/**
+ * Created by woitee on 14/01/2017.
+ */
+
+class KeyboardPlayerController: IPlayerController {
+    class KeyboardHelper: KeyAdapter() {
+        var pressedKeys = HashSet<Int>()
+        var pressedSinceLastUpdate = HashSet<Int>()
+
+        override fun keyPressed(e: KeyEvent?) {
+            if (e != null) {
+                pressedKeys.add(e.keyCode)
+                pressedSinceLastUpdate.add(e.keyCode)
+            }
+        }
+
+        override fun keyReleased(e: KeyEvent?) {
+            if (e != null) {
+                pressedKeys.remove(e.keyCode)
+            }
+        }
+
+        public fun getKeys(): Set<Int> {
+            val res = pressedSinceLastUpdate
+            res.addAll(pressedKeys)
+            pressedSinceLastUpdate = HashSet<Int>()
+            return res
+        }
+    }
+    val keyboardHelper = KeyboardHelper()
+
+    var inited = false
+    fun init(game: Game) {
+        if (game.visualizer == null) {
+            throw InstantiationException("Keyboard Controller can't be set up on games without visualization")
+        }
+        game.visualizer.addKeyListener(keyboardHelper)
+    }
+
+    override fun onUpdate(gameState: GameState): IGameAction? {
+        if (!inited) {
+            init(gameState.game)
+            inited = true
+        }
+        val keys = keyboardHelper.getKeys()
+        if (KeyEvent.VK_UP in keys) {
+            return JumpAction()
+        }
+        return null
+    }
+}
